52eec2dcdc1ba1f467ac7e1b21857ae8839497d6
NiYuiR <162322133+cdsldfds@users.noreply.github.com>
2024-10-13 19:27:45 +0800

    Create About.js

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About This Application</h1>
+      <p>This application is built with React and demonstrates the basics of a single-page application (SPA).</p>
+    </div>
+  );
+}
+
+export default About;
